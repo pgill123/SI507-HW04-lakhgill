a6bf2b229da6ec6bc217e4d489c379793f606434
Preeti Gill <PGill@mathematica.net>
2018-01-30 12:43:36 -0500

    Trying to turn things back to assignment directions after accidentally adding changes to the other branch!

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,5 @@
 # Purpose: Homework 4
-# Collaborators: Preeti Gill,
+# Collaborators: Preeti Gill, Wei Yun Chang,
 
 from random import randrange
 
@@ -25,7 +25,7 @@ magic = [
 ,'Very doubtful'
 ]
 
-print("Possible responses:",magic)
+# print("Possible responses:",magic)
 
 def question ():
     input_answer = ""
@@ -33,6 +33,22 @@ def question ():
         print("****************")
         print ("It's Magic Eight Ball time!")
         input_answer = input('Please enter your question for the Magic Eight Ball or "Q" to quit:\n')
-        # Person B, I think you need to add your code here?
+        #Person A : Write code that checks if user input is a question (i.e., ends in a ‘?’) and,
+        #if not, prints “I’m sorry, I can only answer questions.” (or something similar)
+        #Edit the program so that it keeps asking for user input until the user inputs “quit”
+        if "?" not in input_answer:
+            while (input_answer.upper() != "Q" and  "?" not in input_answer):
+                input_answer = input('I am sorry but I can only answer questions. Try again or enter "Q" to quit:\n')
+        # Person B :　In the magic_eight.py file add code to pick an answer at random from the 20 possible 8 ball answers.
+        # print("Your Question is: " + input_answer + ".")
+        if input_answer == "Q":
+            break
+        random_answer = None
+        num = randrange(0, 19)
+        random_answer = magic[num]
+        print("The answer is: " + random_answer + ".")
+
+
+
 
 test = question()
